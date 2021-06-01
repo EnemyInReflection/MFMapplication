e9ae9ae20c25862402d07960a983856420967902
sadstatue.socnet <sadstatue.socnet@outlook.com>
2021-06-01 19:00:45 +0300

    First commit.

---
+++ src/main/java/com/andreyzuenkov/mfm/MfmApplication.java
@@ -0,0 +1,13 @@
+package com.andreyzuenkov.mfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfmApplication.class, args);
+	}
+
+}
