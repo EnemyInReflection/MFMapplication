05f2f62b0d4fdecc3e960a88b1416b4c858e784b
Andrey Sadman <sadstatue.socnet@gmail.com>
2021-06-02 14:01:16 +0300

    adding message

--- src/main/java/com/andreyzuenkov/mfm/controllers/TestController.java
+++ src/main/java/com/andreyzuenkov/mfm/controllers/TestController.java
@@ -1,15 +1,38 @@
 package com.andreyzuenkov.mfm.controllers;
 
+import com.andreyzuenkov.mfm.domain.Message;
+import com.andreyzuenkov.mfm.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
+    @Autowired
+    private MessageRepo messageRepo;
 
 
     @GetMapping
-    public String testPage(){
+    public String main(Model model) {
 
-        return "index";
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping()
+    public String add(@RequestParam String text, @RequestParam String tag, Model model) {
+
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
     }
 }
